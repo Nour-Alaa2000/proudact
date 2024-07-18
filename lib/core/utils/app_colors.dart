0e6154475374660b0ee3911d1be6c27c94f01d86
nour <nouralaa656303@gmail.com>
2024-07-18 05:50:08 +0300

    init2

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 class AppColors {
-  static const Color primary = Color(0xFF004182);
+  static const Color blue = Color(0xFF004182);
   static const Color onPrimary = Color(0xFFffffff);
-  static const Color hintColor = Color(0x9906004F);
+  static const Color light_blue = Color(0x9906004F);
 }
